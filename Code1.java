a47ef132f776d63b40d0a89f163370d0811c377d
M_O_H_I_T3456 <61095721+mohit7386@users.noreply.github.com>
2023-02-01 09:17:42 +0530

    Changes Committed

--- Code1.java
+++ Code1.java
@@ -1,8 +1,6 @@
 public class Code1{
-    public static void main(String...args) {
+    public static void main(String[]args){
         System.out.println("Hey Jarvis!");
         System.out.print("Hello Sir How Are You!");
-
-    
-    }
+}
 }
