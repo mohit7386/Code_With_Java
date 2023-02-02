3058410ef57ea9d4f3de5f7b3d9a271b74e3064b
M_O_H_I_T3456 <61095721+mohit7386@users.noreply.github.com>
2023-02-02 22:06:04 +0530

    Changes Committed

--- Code1.java
+++ Code1.java
@@ -1,6 +1,11 @@
 public class Code1{
-    public static void main(String[]args){
+    public static void main(String ... args){
         System.out.println("Hey Jarvis!");
-        System.out.print("Hello Sir How Are You!");
+        System.out.println("Hello Sir How Are You!");
+        System.out.println(3+3);
+        System.out.println(5*5);
+        System.out.println(4-3);
+        System.out.println(6/3);
+        System.out.println(9%4);
 }
 }
--- readme.md
+++ readme.md
@@ -138,3 +138,5 @@ The JVM performs following operations:-->
 
 
 
+
+
