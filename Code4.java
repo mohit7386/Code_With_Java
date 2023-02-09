73711564dc805d6f2722bbc17bbca2d46910bf3a
M_O_H_I_T3456 <61095721+mohit7386@users.noreply.github.com>
2023-02-09 08:40:54 +0530

    Changes committed

---
+++ Code4.java
@@ -0,0 +1,11 @@
+public class Code4 {
+    public static void main(String...args){
+        int a=29;
+        float b=48f;
+        float sum=a+b; // We cannot Convert from float to int in java 
+        System.out.println(sum);
+        System.out.println(b-a);
+        System.out.println(b/a);
+
+    } 
+}
